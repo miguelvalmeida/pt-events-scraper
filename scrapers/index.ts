36db140f75673ba6476b444483c6c0e993c290ea
Miguel Almeida <miguelvalmeida26@gmail.com>
2025-08-07 11:57:03 +0100

    Scrape egeac

--- scrapers/index.ts
+++ scrapers/index.ts
@@ -2,4 +2,5 @@ import { scrapeAgendaCulturalPorto } from "./agenda-cultural-porto";
 
 export const scrapers = [
   { source: "agenda-cultural-porto", fn: scrapeAgendaCulturalPorto },
+  { source: "egeac", fn: scrapeAgendaCulturalPorto },
 ];
---
+++ scrapers/lisboa-cultura.ts
@@ -0,0 +1,36 @@
+import { chromium } from "playwright";
+
+export async function scrapeAgendaCulturalPorto() {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto("https://egeac.pt/programacao-espacos-culturais/", {
+    waitUntil: "domcontentloaded",
+  });
+
+  const events = await page.$$eval(".evento-card", (cards) => {
+    return cards.map((card) => {
+      const titleEl = card.querySelector(".h5");
+      const dateEl = card.querySelector(".date");
+      const locationEl = card.querySelector(".location");
+      const urlEl = card.querySelector("a");
+      const imageEl = card.querySelector("img");
+
+      return {
+        title: titleEl?.textContent?.trim() || null,
+        url: urlEl?.getAttribute("href") || null,
+        date: dateEl?.textContent?.trim() || null,
+        location: locationEl?.textContent?.trim() || null,
+        image: imageEl?.getAttribute("src") || null,
+      };
+    });
+  });
+
+  await browser.close();
+
+  console.log(events);
+
+  return events;
+}
+
+scrapeAgendaCulturalPorto();
